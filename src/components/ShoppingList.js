768f5a456d619c54c1b8313b8bcb822df95b5390
Eric093 <eric.blot1@gmail.com>
2025-01-12 18:55:21 +0100

    Refactor components and styles: update ShoppingList layout, add CareScale component, and modify Banner styles

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,4 +1,6 @@
 import { plantList } from "../datas/plantList";
+import CareScale from "./CareScale";
+import "../styles/ShoppingList.css";
 
 function ShoppingList() {
   const categories = plantList.reduce(
@@ -14,11 +16,13 @@ function ShoppingList() {
           <li key={cat}>{cat}</li>
         ))}
       </ul>
-      <ul>
+      <ul className="lmj-plant-list">
         {plantList.map((plant) => (
-          <li key={plant.id}>
+          <li key={plant.id} className="lmj-plant-item">
+            {plant.isBestSale && <span>🔥</span>}
             {plant.name}
-            {plant.isBestSale ? <span>🔥</span> : <span>👎</span>}
+            <CareScale careType="water" scaleValue={plant.water} />
+            <CareScale careType="light" scaleValue={plant.light} />
           </li>
         ))}
       </ul>
